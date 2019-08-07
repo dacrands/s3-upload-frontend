ec52e9adf0667e978898391ffb4556ca5e431b3f
da crands <davecrands@gmail.com>
2019-08-07 09:18:38 -0400

    Fix production BASE_URL

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,3 +1,3 @@
 export const BASE_URL = process.env.NODE_ENV === "development" 
                         ? "http://localhost:5000" 
-                        : "https://files.crandal.dev"
+                        : "https://api.files.crandall.dev"
