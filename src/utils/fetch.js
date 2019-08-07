3a902493ddcdf9600a8cef912dea671946076b36
da crands <davecrands@gmail.com>
2019-08-07 09:17:58 -0400

    Revert "Fix production BASE_URL"
    This reverts commit 57c1223771f31362b0200424a255499b68136110.

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,3 +1,3 @@
 export const BASE_URL = process.env.NODE_ENV === "development" 
                         ? "http://localhost:5000" 
-                        : "https://files.crandall.dev"
+                        : "https://files.crandal.dev"
