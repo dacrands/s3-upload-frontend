12474bca3d50aa624ee3bcf5aa287f035726dcfb
da crands <davecrands@gmail.com>
2020-03-04 18:09:16 -0500

    Add CSRF helpers

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,3 +2,33 @@ export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
     : 'https://api.files.crandall.dev'
+
+
+export const setCSRF = token => {
+  try {
+    window.localStorage.setItem('csrf' , token)    
+  } catch (e) {
+    alert('There was an error setting your CSRF token.')
+    console.log(e)
+  }
+}
+
+export const getCSRF = () => {
+  try {
+    return window.localStorage.csrf
+  } catch (e) {
+    alert('Unable to retrieve CSRF token from local storage.')
+    console.log(e)
+  }
+}
+
+export const deleteCSRF = () => {
+  try {
+    window.localStorage.removeItem('csrf')    
+  } catch (e) {
+    alert('There was an error removing your CSRF token from local storage.')
+    console.log(e)
+  }
+}
+
+export default { BASE_URL, setCSRF, getCSRF, deleteCSRF }
